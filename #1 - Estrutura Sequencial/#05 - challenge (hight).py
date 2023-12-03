fe93cf8c0ad47bfe02ba6e8b042a9db4e19beca4
Brandon <josephsilvak@gmail.com>
2023-12-03 01:15:22 -0300

    corrige algoritmo 5

--- #1 - Estrutura Sequencial/#05 - challenge (hight).py
+++ #1 - Estrutura Sequencial/#05 - challenge (hight).py
@@ -1,10 +1,10 @@
 # 5 - Faça um Programa que converta metros para centímetros.
-
-while True:
+centimetro = None
+while centimetro == None:
     try:
-        num = float(input("Informe um tamanho em \033[0;36mMetro\033[m: "))
-        print(f"{num}m é equivalente a {num * 1000}cm")
-        break
+        metro = float(input("Informe um tamanho em \033[0;36mMetro\033[m: "))
+        centimetro = metro * 1000
+        print(f"{metro}m é equivalente a {centimetro}cm")
     except:
         print('\033[0;31mO valor informado não é um número.\033[m')
         continue
